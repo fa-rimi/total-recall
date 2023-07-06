b3b11c97d01400f1b3c8473ecda10793f05ab93a
rimi <a.fatimaarimi@gmail.com>
2023-07-06 17:57:05 -0400

    Answered question 3&4 of part A

--- w4d2.js
+++ w4d2.js
@@ -11,11 +11,18 @@
  * 2. How do we change the value of a variable?
  *      A: We change the value of a variable by using the assignment operator (=) 
  *      Ex: let colossalTitan = "Bertolt";
- *          colossalTitan = "Armin"
+ *          colossalTitan = "Armin";
  * 
  * 3. How do we assign an existing variable to a new variable?
+ *      A: We can assign an existing variable to a new variable using the assignment operator (=)
+ *      Ex: let a = 5; 
+ *          b = a;
  * 
  * 4. Remind me, what are declare, assign, and define?
+ *      A: Declaring is declaring a variable with a keyword and variable name. (let a;)
+ *         Assigning is to assign a value to said variable. (let a = "sighs";)
+ *         Defining is telling the computer that there is a variable that exists with a specific value and type so the program can then
+ *         proceed and compile.
  * 
  * 5. What is pseudo coding and why should you do it?
  * 
