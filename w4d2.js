70bb5369f3f62381d6fe622b085c00d97165c90b
rimi <a.fatimaarimi@gmail.com>
2023-07-06 18:51:13 -0400

    completed Part C - Booleans

--- w4d2.js
+++ w4d2.js
@@ -1,6 +1,6 @@
 // JavaScript Review Lab
 
-// Part A - Q and A
+// PART A - Q/A
 /**
  * 1. How do we assign a value to a variable?
  *      A: There are different ways to assign a value to a variable depending on the language. For JS specifically you would would
@@ -37,7 +37,7 @@
  *
  */
 
-// Part B - Strings
+//  PART B - STRINGS
 
 // Create a variable called firstVariable
 // const firstVariable;
@@ -61,3 +61,27 @@ console.log(firstVariable);
 // Create a variable called yourName and set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
 let yourName = "Rimi";
 console.log(`Hello, my name is ${yourName}`);
+
+// PART C - BOOLEANS
+// Using the provided variable definitions, replace the blanks so that all log statements print true in the console. Answers should be all be valid JS syntax and not weird things that don't make sense but happen to print true to the console
+
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
+
+console.log(a < b);
+console.log(c > d);
+console.log('Name' === 'Name');
+// FOR THE NEXT TWO, USE ONLY && OR ||
+console.log(true || false);
+console.log(false && false && false && false && false || true);
+console.log(false === false)
+console.log(e === 'Kevin');
+console.log(a + b === c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+console.log(a * a === d); // note: the answer is a simple arithmetic equation, not something "weird"
+console.log(48 !== '48');
+
+// PART D - THE FARM
+// PART E - DRIVERS ED
