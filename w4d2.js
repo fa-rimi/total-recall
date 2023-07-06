f9ec019c7b85c525b957a205a971b06d688378e3
rimi <a.fatimaarimi@gmail.com>
2023-07-06 17:27:44 -0400

    answered question 1 for part A

---
+++ w4d2.js
@@ -0,0 +1,21 @@
+// JavaScript Review Lab
+
+// A - Q and A
+/**
+ * 1. How do we assign a value to a variable?
+ *      A: There are different ways to assign a value to a variable depending on the language. For JS specifically you would would
+ *         declare the variable by combining a keyword and variable name. Then you would equal(=) it to the value. Values could be a 
+ *         string, numbers, booleans, etc. 
+ *      Ex: const hermione = "ron";
+ * 
+ * 2. How do we change the value of a variable?
+ * 
+ * 3. How do we assign an existing variable to a new variable?
+ * 
+ * 4. Remind me, what are declare, assign, and define?
+ * 
+ * 5. What is pseudo coding and why should you do it?
+ * 
+ * 6. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+ * 
+ */
