c95899c96586813370bdf09e6ae6c0c27e18258c
rimi <a.fatimaarimi@gmail.com>
2023-07-09 20:15:13 -0400

    completed section A of  PART 2

--- w4d2.js
+++ w4d2.js
@@ -1,5 +1,7 @@
 // JavaScript Review Lab
 
+// PART I
+
 // PART A - Q/A
 /**
  * 1. How do we assign a value to a variable?
@@ -117,5 +119,32 @@ if (age >= 16) {
 } else {
   console.log("invalid age");
 }
-
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// PART II
+
+/**
+ * Remember: USE letwhen you initialize your for loops!
+ *
+ * This is GOOD: for(let i = 0; i < 100; i++)
+ * This is NO GOOD: for(i = 0; i < 100; i++)
+ *
+ **/
+
+// A -- THE BASICS
+// Write a loop that will print out all the numbers from 0 to 10, inclusive
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+// Write a loop that will print out all the numbers from 10 up to and including 400
+for (let r = 10; r <= 400; r++) {
+  console.log(r);
+}
+
+// Write a loop that will print out every third number starting with 12 and going no higher than 4000
+for (let num = 12; num <= 4000; num++) {
+  if (num % 3 === 0) {
+    console.log(num);
+  }
+}
