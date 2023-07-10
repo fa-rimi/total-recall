b06097117cd3b1077b5bb9b0432b3b273b286c22
rimi <a.fatimaarimi@gmail.com>
2023-07-09 23:28:54 -0400

    completed section D of PART 2

--- w4d2.js
+++ w4d2.js
@@ -180,9 +180,22 @@ for (let index = 0; index <= 100; index++) {
 
 // D -- SAVINGS ACCOUNT
 // Write code that will save the sum of all the numbers between 1 - 10 to a variable called bank_account.
+let bank_account = 0;
 
+// for( let i= 1; i <= 10; i++){
+//     bank_account += i
+// }
+// console.log(bank_account)
 // Check your work! Your bank_account should have $55 in it.
 
 // You got a bonus! Your pay is now doubled each week. Write code that will save the sum of all the numbers between 1 - 100 multiplied by 2.
-
+for (let i = 1; i <= 100; i++) {
+  bank_account += i;
+  if (i === 100) {
+    bank_account *= 2;
+  }
+}
+console.log(bank_account);
 //Check your work! Your bank_account should have $10,100 in it.
+
+
